e3f413a90621df780fa68ee780ea11cb68117625
Nicky Case <n@ncase.me>
2014-05-18 16:52:06 -0700

    Why sections parallax with image

--- js/index.js
+++ js/index.js
@@ -1,18 +1,26 @@
 window.onload = function(){
 
-	// THE BANNER LAYERS - PARALLAX
-	var layers = document.querySelectorAll("#splash > div");
+	// PARALLAX
+	var splashLayers = document.querySelectorAll("#splash > div");
+	var whyBackgrounds = document.querySelectorAll("#why_background");
 	window.onscroll = function(){
 
 		// Scroll Offset
 		var scrollTop = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
 
-		// Move all layers
-		for(var i=0;i<layers.length;i++){
-			var layer = layers[i];
-			var y = (scrollTop*(1-Math.pow(0.9,((layers.length+2)-i))))+"px";
-			var x = (i==layers.length-1) ? "50%" : "calc(50% + 250px)";
+		// Splash Layers
+		for(var i=0;i<splashLayers.length;i++){
+			var layer = splashLayers[i];
+			var y = (scrollTop*(1-Math.pow(0.9,((splashLayers.length+2)-i))))+"px";
+			var x = (i==splashLayers.length-1) ? "50%" : "calc(50% + 250px)";
 			layer.style.backgroundPosition = x+" "+y;
+		}
+
+		// Why Background
+		for(var i=0;i<whyBackgrounds.length;i++){
+			var layer = whyBackgrounds[i];
+			var y = (scrollTop-layer.offsetTop)*0.5;
+			layer.style.backgroundPosition = "0px "+y+"px";
 		}
 
 	};
