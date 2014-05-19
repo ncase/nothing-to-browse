b3ed9dfa760109c53c577587a0a916188a3d24c8
Nicky Case <n@ncase.me>
2014-05-18 18:52:04 -0700

    Piwik analytics and Parallax script kicks in immediately

--- static/js/index.js
+++ static/js/index.js
@@ -1,35 +1,31 @@
-window.onload = function(){
+// PARALLAX
+var splashLayers = document.querySelectorAll("#splash > div");
+var whyBackgrounds = document.querySelectorAll("#why_background");
 
-	// PARALLAX
-	var splashLayers = document.querySelectorAll("#splash > div");
-	var whyBackgrounds = document.querySelectorAll("#why_background");
+// Render
+var render = function(){
 
-	// Render
-	var render = function(){
+	// Scroll Offset
+	var scrollTop = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
 
-		// Scroll Offset
-		var scrollTop = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+	// Splash Layers
+	for(var i=0;i<splashLayers.length;i++){
+		var layer = splashLayers[i];
+		var y = (scrollTop*(1-Math.pow(0.9,((splashLayers.length+2)-i))))+"px";
+		var x = (i==splashLayers.length-1) ? "50%" : "calc(50% + 250px)";
+		layer.style.backgroundPosition = x+" "+y;
+	}
 
-		// Splash Layers
-		for(var i=0;i<splashLayers.length;i++){
-			var layer = splashLayers[i];
-			var y = (scrollTop*(1-Math.pow(0.9,((splashLayers.length+2)-i))))+"px";
-			var x = (i==splashLayers.length-1) ? "50%" : "calc(50% + 250px)";
-			layer.style.backgroundPosition = x+" "+y;
-		}
+	// Why Background
+	for(var i=0;i<whyBackgrounds.length;i++){
+		var layer = whyBackgrounds[i];
+		var y = (scrollTop-layer.offsetTop)*0.5;
+		layer.style.backgroundPosition = "0px "+y+"px";
+	}
 
-		// Why Background
-		for(var i=0;i<whyBackgrounds.length;i++){
-			var layer = whyBackgrounds[i];
-			var y = (scrollTop-layer.offsetTop)*0.5;
-			layer.style.backgroundPosition = "0px "+y+"px";
-		}
+};
 
-	};
-
-	// Re-render whenever scrolling
-	window.onscroll = function(){
-		render();
-	};
-
+// Re-render whenever scrolling
+window.onscroll = function(){
+	render();
 };
